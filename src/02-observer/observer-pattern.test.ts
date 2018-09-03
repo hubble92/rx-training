697af4c9fba2b6e3754f7f6b68971ae5f7bb7e68
archegalla <artempushkar@gmail.com>
2018-09-03 21:25:07 +0300

    update test on Observer Pattern

--- src/02-observer/observer-pattern.test.ts
+++ src/02-observer/observer-pattern.test.ts
@@ -11,3 +11,25 @@ beforeEach(() => {
 test("Instance should be defined", () => {
     expect(subject).toBeDefined();
 });
+
+test("Observer instance should has method update", () => {
+    const observer: Observer = new Developer(subject);
+
+    expect(observer.update).toBeDefined();
+});
+
+test("On state change observers should be notifyed", () => {
+    const developerOne = new Developer(subject);
+    const developerTwo = new Developer(subject);
+    const developerThree = new Developer(subject);
+
+    const spyOne = jest.spyOn(developerOne, "update");
+    const spyTwo = jest.spyOn(developerTwo, "update");
+    const spyThree = jest.spyOn(developerThree, "update");
+
+    subject.setState({tasks: ["test"]});
+
+    expect(spyOne).toBeCalled();
+    expect(spyTwo).toBeCalled();
+    expect(spyThree).toBeCalled();
+});
