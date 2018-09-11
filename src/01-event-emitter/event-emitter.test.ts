7b84d88c9f7dd7ce2e405d6e5d9ce536ceb86ae5
artem.pushkar <artem.pushkar@harmonicinc.com>
2018-09-11 12:58:37 +0300

    fix test

--- src/01-event-emitter/event-emitter.test.ts
+++ src/01-event-emitter/event-emitter.test.ts
@@ -1,53 +1,46 @@
-import { EventEmitter } from "./event-emitter";
-import { IListener } from "./iface";
+import {EventEmitter} from "./event-emitter";
+import {IListener} from "./iface";
 
 let emitter: EventEmitter;
 let listener: IListener;
 let listener2: IListener;
 
-fdescribe('#EventEmitter', () => {
-    beforeEach(() => {
-        emitter = new EventEmitter();
+beforeEach(() => {
+    emitter = new EventEmitter();
 
-        listener = (eventData: any) => console.log(`hello ${eventData}`);
-        listener2 = (eventData: any) => console.log(`hello ${eventData}`);
-    });
+    listener = (eventData: any) => console.log(`hello ${eventData}`);
+    listener2 = (eventData: any) => console.log(`hello ${eventData}`);
+});
 
-    test("should be defined with methods", () => {
-        expect(emitter).toBeDefined();
-        expect(emitter.emit).toBeDefined();
-        expect(emitter.addEventListener).toBeDefined();
-        expect(emitter.removeEventListener).toBeDefined();
-    });
+test("should be defined with methods", () => {
+    expect(emitter).toBeDefined();
+    expect(emitter.emit).toBeDefined();
+    expect(emitter.addEventListener).toBeDefined();
+    expect(emitter.removeEventListener).toBeDefined();
+});
 
-    describe('#addEventListener', () => {
-        emitter = new EventEmitter();
+test("should add listeners to list", () => {
+    emitter = new EventEmitter();
 
-        listener = (eventData: any) => console.log(`hello ${eventData}`);
-        listener2 = (eventData: any) => console.log(`hello ${eventData}`);
+    listener = (eventData: any) => console.log(`hello ${eventData}`);
+    listener2 = (eventData: any) => console.log(`hello ${eventData}`);
 
-        emitter.addEventListener('click', listener);
-        emitter.addEventListener('click', listener2);
-        console.log('emitter.events', emitter.events);
-        console.log('emitter.events["click"].length = ', emitter.events['click'].length);
+    emitter.addEventListener("click", listener);
+    emitter.addEventListener("click", listener2);
 
-        test("should add listeners to list", () => {
-            expect(emitter.events['click'].length).toEqual(2);
-        });
-    });
+    expect(emitter.events.click.length).toEqual(2);
+});
 
-    describe('#removeEventListener', () => {
-        emitter = new EventEmitter();
-        listener = (eventData: any) => console.log(`hello ${eventData}`);
-        listener2 = (eventData: any) => console.log(`hello ${eventData}`);
+test("should remove listeners to list", () => {
+    emitter = new EventEmitter();
 
+    listener = (eventData: any) => console.log(`hello ${eventData}`);
+    listener2 = (eventData: any) => console.log(`hello ${eventData}`);
 
-        emitter.addEventListener('click', listener);
-        emitter.addEventListener('click', listener2);
-        emitter.removeEventListener('click', listener2);
+    emitter.addEventListener("click", listener);
+    emitter.addEventListener("click", listener2);
 
-        test("should remove listeners to list", () => {
-            expect(emitter.events['click'].length).toEqual(1);
-        });
-    });
+    emitter.removeEventListener("click", listener2);
+
+    expect(emitter.events.click.length).toEqual(1);
 });
--- src/01-event-emitter/event-emitter.ts
+++ src/01-event-emitter/event-emitter.ts
@@ -1,10 +1,10 @@
-import { Event, IListener, IEventEmitter, IEventData } from "./iface";
+import { Event, IEventData, IEventEmitter, IListener } from "./iface";
 
 export class EventEmitter implements IEventEmitter {
     public events: {[key: string]: IListener[]} = {};
 
-    addEventListener(event: Event, listener: IListener): void {
-        if(this.events[event]) {
+    public addEventListener(event: Event, listener: IListener): void {
+        if (this.events[event]) {
             this.events[event].push(listener);
         } else {
             this.events[event] = [];
@@ -12,39 +12,39 @@ export class EventEmitter implements IEventEmitter {
         }
     }
 
-    removeEventListener(event: Event, listener: IListener): void {
+    public removeEventListener(event: Event, listener: IListener): void {
         if (this.events[event]) {
             this.events[event] = this.events[event]
                 .filter((l: IListener) => l !== listener);
         }
     }
 
-    emit(event: Event, eventData: IEventData ): void {
+    public emit(event: Event, eventData: IEventData ): void {
         if (this.events[event]) {
-            for(const listener of this.events[event]){
+            for (const listener of this.events[event]) {
                 listener(eventData);
             }
         }
     }
 }
-
-(function () {
-    const eventEmiter =  new EventEmitter();
-
-    const listener = (eventData: any) => console.log(`Hello from first ${eventData}`);
-    const listener2 = (eventData: any) => console.log(`Hello from second ${eventData}`);
-
-    eventEmiter.addEventListener('hello', listener);
-    eventEmiter.addEventListener('hello', listener2);
-
-    eventEmiter.emit('hello', 'listener');
-
-    eventEmiter.removeEventListener('hello', listener);
-
-    // only second listener will fire
-    eventEmiter.emit('hello', 'listener');
-
-})();
+//
+// (() => {
+//     const eventEmitter =  new EventEmitter();
+//
+//     const listener = (eventData: any) => console.log(`Hello from first ${eventData}`);
+//     const listener2 = (eventData: any) => console.log(`Hello from second ${eventData}`);
+//
+//     eventEmitter.addEventListener("hello", listener);
+//     eventEmitter.addEventListener("hello", listener2);
+//
+//     eventEmitter.emit("hello", "listener");
+//
+//     eventEmitter.removeEventListener("hello", listener);
+//
+//     // only second listener will fire
+//     eventEmitter.emit("hello", "listener");
+//
+// })();
 
 
 
